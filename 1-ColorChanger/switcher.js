930ffb48e5eacdeb68e452f94167a10e4511e983
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-17 22:16:38 +0500

    if stament apply

--- 1-ColorChanger/switcher.js
+++ 1-ColorChanger/switcher.js
@@ -12,6 +12,16 @@ buttons.forEach(function (button) {
         if (e.target.id === 'white') {
             body.style.backgroundColor = e.target.id
         }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'pink') {
+            body.style.backgroundColor = e.target.id
+        }
+
 
 
     });
