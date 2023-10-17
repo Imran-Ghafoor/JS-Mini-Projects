af7cc28126373403428e04e576dc80b18f8d2318
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-17 22:11:44 +0500

    foreach loop

--- 1-ColorChanger/switcher.js
+++ 1-ColorChanger/switcher.js
@@ -1,3 +1,7 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 // console.log(buttons);
+buttons.forEach(function (button) {
+    console.log(button);
+
+});
