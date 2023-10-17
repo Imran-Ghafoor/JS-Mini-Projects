8b1dd00b63a6c7357a41573bf0a5fe8f10ea6c2c
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-17 22:14:41 +0500

    addEventListener for event

--- 1-ColorChanger/switcher.js
+++ 1-ColorChanger/switcher.js
@@ -2,6 +2,10 @@ const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 // console.log(buttons);
 buttons.forEach(function (button) {
-    // console.log(button);
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+
+    });
 
 });
