f3ce98cdfb3e0e03dba8ec41f1feb57ff84ffb5a
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-17 22:15:56 +0500

    apply if condition for apply color

--- 1-ColorChanger/switcher.js
+++ 1-ColorChanger/switcher.js
@@ -5,6 +5,14 @@ buttons.forEach(function (button) {
     console.log(button);
     button.addEventListener('click', function (e) {
         console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id
+        }
+
 
     });
 
