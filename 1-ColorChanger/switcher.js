c9c11fc4ae32b3a1e9a24dcb4aaddce38b8fbd10
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-17 22:12:45 +0500

    applay loop on all button select

--- 1-ColorChanger/switcher.js
+++ 1-ColorChanger/switcher.js
@@ -2,6 +2,6 @@ const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 // console.log(buttons);
 buttons.forEach(function (button) {
-    console.log(button);
+    // console.log(button);
 
 });
