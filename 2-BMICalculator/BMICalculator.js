6c010cae581aaf389a145081b4e07fdfb5342d2d
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-19 00:43:34 +0500

    Functionality

--- 2-BMICalculator/BMICalculator.js
+++ 2-BMICalculator/BMICalculator.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('form')
 form.addEventListener('submit' , function(e){
     e.preventDefault()
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
 })
