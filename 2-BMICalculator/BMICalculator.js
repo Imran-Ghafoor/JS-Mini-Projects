80c0c143cd9f93499ea3d5b386ffa576147d3049
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-19 00:55:31 +0500

    validations

--- 2-BMICalculator/BMICalculator.js
+++ 2-BMICalculator/BMICalculator.js
@@ -6,7 +6,10 @@ form.addEventListener('submit', function (e) {
     e.preventDefault()
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
-    const results = (document.querySelector('#results').value)
+    const results = document.querySelector('#results')
 
-    
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = "please give a valid Height";
+
+    }
 })
