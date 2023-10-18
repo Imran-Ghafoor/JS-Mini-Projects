c797b1f789410bc3b1d3aa58920e4c63bdd63cef
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-19 00:50:13 +0500

    result value

--- 2-BMICalculator/BMICalculator.js
+++ 2-BMICalculator/BMICalculator.js
@@ -6,4 +6,7 @@ form.addEventListener('submit', function (e) {
     e.preventDefault()
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
+    const results = (document.querySelector('#results').value)
+
+    
 })
