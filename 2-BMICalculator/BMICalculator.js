6e791decb038737f8bc0379648bba78d643b54bf
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-19 01:23:56 +0500

    function solution

--- 2-BMICalculator/BMICalculator.js
+++ 2-BMICalculator/BMICalculator.js
@@ -11,11 +11,10 @@ form.addEventListener('submit', function (e) {
     if (height === '' || height < 0 || isNaN(height)) {
         results.innerHTML = `Please give a valid Height ${height}`;
 
-    }
-    else if (weight === '' || weight < 0 || isNaN(weight)) {
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
         results.innerHTML = `Please give a valid Weight ${weight}`;
     } else {
-        (weight / ((height * height / 1000)).toFixed(2))
+        const bmi = (weight / ((height * height / 1000)).toFixed(2));
 
         //show the result
         results.innerHTML = `<span>${bmi}</span>`
