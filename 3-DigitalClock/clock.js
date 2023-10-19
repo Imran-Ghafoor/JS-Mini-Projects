2c85003ef3502a6e7d03805e8d2164f83b51c847
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-19 21:27:24 +0500

    Project 3 js- function

---
+++ 3-DigitalClock/clock.js
@@ -0,0 +1,9 @@
+const clock = document.getElementById('clock');
+// const cock = document.querySelector('clock')
+
+setInterval(function () {
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString()
+    // console.log(clock);
+}, 1000);
