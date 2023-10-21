9a77dd57abed4d6d7e9790ee69f6cdd23ed6c92d
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-10-21 21:55:46 +0500

    GuessTheNumber

---
+++ 4-GuessTheNumber/function.js
@@ -0,0 +1,43 @@
+const randomNumber = (parseInt(Math.random() * 100 + 1));
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessfield');
+const guessSlot = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const paragraph = document.createElement('p');
+let prevGuess = [];
+let numGuess = 1;
+let playGmae = true;
+
+// Methods
+
+function validGuess(guess) {
+    //
+}
+
+function checkGuess(guess) {
+    //
+}
+
+function displayGuess(guess) {
+    //
+}
+
+function displayMessage(message) {
+    //
+}
+
+
+function endGame() {
+    //
+}
+
+
+function newGame() {
+    //
+}
+
+
+
